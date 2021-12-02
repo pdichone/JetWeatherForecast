ba3f9bdec95844a68ac3734cd8689f4d6046894b
Paulo <pdichone@gmail.com>
2021-12-02 08:50:00 -0800

    Update Constants

--- app/src/main/java/com/bawp/jetweatherforecast/utils/Constants.kt
+++ app/src/main/java/com/bawp/jetweatherforecast/utils/Constants.kt
@@ -2,6 +2,5 @@ package com.bawp.jetweatherforecast.utils
 
 object Constants {
     const val   BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
-   // const val API_KEY = "ADD_YOUR_OWN_APPID_HERE"
+    const val API_KEY = "ADD_YOUR_OWN_APPID_HERE" // go to: https://openweathermap.org/forecast16
 }
